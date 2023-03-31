5d64464ae1dded7da59943f35ce2cc2d424dadce
Matthew Hillis <843288+hillis@users.noreply.github.com>
2023-03-31 08:04:49 -0500

    Update Document Loader to GitHub Repo

--- ingest.ts
+++ ingest.ts
@@ -1,16 +1,16 @@
 import { HNSWLib } from "langchain/vectorstores";
 import { OpenAIEmbeddings } from "langchain/embeddings";
 import { RecursiveCharacterTextSplitter } from "langchain/text_splitter";
-import { TextLoader, DirectoryLoader, PDFLoader } from "langchain/document_loaders";
+import { TextLoader, DirectoryLoader, PDFLoader, GithubRepoLoader } from "langchain/document_loaders";
 
 const FILENAME = "./data/limits2023.md";
 
 
 export const run = async () => {
-  const loader = new DirectoryLoader("sportsdata", {
-    ".md": (path) => new TextLoader(path),
-    ".pdf": (path) => new PDFLoader(path),
-  });
+  const loader = new GithubRepoLoader(
+  "https://github.com/hillis/gpt-4-chat-ui",
+    { branch: "main", recursive: false, unknown: "warn" });
+  
   const rawDocs = await loader.load();
   console.log("Loader created.");
   /* Split the text into chunks */
