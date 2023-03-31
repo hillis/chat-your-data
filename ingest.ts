decce41440fb34e6dc34606b3e6135e4d4a9dc6c
Matthew Hillis <843288+hillis@users.noreply.github.com>
2023-03-31 17:28:11 -0500

    Update langchain  hnswlib.  Refactored ChainValues

--- ingest.ts
+++ ingest.ts
@@ -3,7 +3,7 @@ import { OpenAIEmbeddings } from "langchain/embeddings";
 import { RecursiveCharacterTextSplitter } from "langchain/text_splitter";
 import { TextLoader } from "langchain/document_loaders";
 
-const FILENAME = "./data/limits2023.md";
+const FILENAME = "limits2023.md";
 
 export const run = async () => {
   const loader = new TextLoader(FILENAME);
