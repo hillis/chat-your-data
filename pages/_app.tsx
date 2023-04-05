01aa8a6841a1044ae95d35c63e58b02a2b2fbe99
Matthew Hillis <843288+hillis@users.noreply.github.com>
2023-04-05 15:16:48 -0500

    Updating code

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,6 +1,6 @@
 import '@/styles/globals.css'
 import type { AppProps } from 'next/app'
-import Script from 'next/script'
+
 import { Analytics } from "@vercel/analytics/react";
 
 export default function App({ Component, pageProps }: AppProps) {
