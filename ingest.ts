29219c82a41f997c0c9f3d5d6faea2c0eacb7a77
Matthew Hillis <843288+hillis@users.noreply.github.com>
2023-04-08 22:53:38 -0500

    Add error catch and FilePath const

--- ingest.ts
+++ ingest.ts
@@ -4,27 +4,39 @@ import { RecursiveCharacterTextSplitter } from "langchain/text_splitter";
 import { TextLoader, DirectoryLoader, PDFLoader } from "langchain/document_loaders";
 
 const FILENAME = "./data/limits2023.md";
-
+const filePath = 'docs';
 
 export const run = async () => {
-  const loader = new DirectoryLoader("docs/sportsdata", {
-    ".md": (path) => new TextLoader(path),
-    ".pdf": (path) => new PDFLoader(path),
-  });
-  const rawDocs = await loader.load();
-  console.log("Loader created.");
-  /* Split the text into chunks */
-  const textSplitter = new RecursiveCharacterTextSplitter({
-    chunkSize: 1000,
-    chunkOverlap: 200,
-  });
-  const docs = await textSplitter.splitDocuments(rawDocs);
-  console.log("Docs splitted.");
+  try {
+    const loader = new DirectoryLoader(filePath, {
+      ".md": (path) => new TextLoader(path),
+      ".pdf": (path) => new PDFLoader(path),
+    });
+    
+  
+    const rawDocs = await loader.load();
+    console.log("Loader created.");
+    
 
-  console.log("Creating vector store...");
-  /* Create the vectorstore */
-  const vectorStore = await HNSWLib.fromDocuments(docs, new OpenAIEmbeddings());
-  await vectorStore.save("data");
+    /* Split the text into chunks */
+    const textSplitter = new RecursiveCharacterTextSplitter({
+      chunkSize: 1000,
+      chunkOverlap: 200,
+    });
+    
+    
+    const docs = await textSplitter.splitDocuments(rawDocs);
+    console.log("Docs split.");
+
+    console.log("Creating vector store...");
+    /* Create the vectorstore */
+    const vectorStore = await HNSWLib.fromDocuments(docs, new OpenAIEmbeddings());
+    await vectorStore.save("data");
+  }
+  catch (error) {
+    console.log('error', error);
+    throw new Error('Failed to ingest documents');
+  }
 };
 
 (async () => {
