8892fbd2ed32abfe69321502500d4378f7fc7721
Matthew Hillis <843288+hillis@users.noreply.github.com>
2023-04-09 20:43:24 -0500

    Update Ingest for Pinecone Vectordatabase

--- ingest.ts
+++ ingest.ts
@@ -1,45 +1,52 @@
-import { HNSWLib } from "langchain/vectorstores";
+import { HNSWLib, PineconeStore } from "langchain/vectorstores";
+import { pinecone } from '@/utils/pinecone-client';
+import { PINECONE_INDEX_NAME, PINECONE_NAME_SPACE } from "@/config/pinecone";
 import { OpenAIEmbeddings } from "langchain/embeddings";
 import { RecursiveCharacterTextSplitter } from "langchain/text_splitter";
-import { TextLoader, DirectoryLoader, PDFLoader } from "langchain/document_loaders";
+import { TextLoader, DirectoryLoader } from "langchain/document_loaders";
+import { CustomPDFLoader } from "scripts/customPDFLoader";
 
 const FILENAME = "./data/limits2023.md";
 const filePath = 'docs';
 
 export const run = async () => {
   try {
-    const loader = new DirectoryLoader(filePath, {
+    /*load raw docs from the all files in the directory */
+    const directoryLoader = new DirectoryLoader(filePath, {
+      ".pdf": (path) => new CustomPDFLoader(path),
       ".md": (path) => new TextLoader(path),
-      ".pdf": (path) => new PDFLoader(path),
     });
-    
-  
-    const rawDocs = await loader.load();
-    console.log("Loader created.");
-    
 
-    /* Split the text into chunks */
+    // const loader = new PDFLoader(filePath);
+    const rawDocs = await directoryLoader.load();
+
+    /* Split text into chunks */
     const textSplitter = new RecursiveCharacterTextSplitter({
       chunkSize: 1000,
       chunkOverlap: 200,
     });
-    
-    
-    const docs = await textSplitter.splitDocuments(rawDocs);
-    console.log("Docs split.");
 
-    console.log("Creating vector store...");
-    /* Create the vectorstore */
-    const vectorStore = await HNSWLib.fromDocuments(docs, new OpenAIEmbeddings());
-    await vectorStore.save("data");
-  }
-  catch (error) {
+    const docs = await textSplitter.splitDocuments(rawDocs);
+    console.log('split docs', docs);
+
+    console.log('creating vector store...');
+    /*create and store the embeddings in the vectorStore*/
+    const embeddings = new OpenAIEmbeddings();
+    const index = pinecone.Index(PINECONE_INDEX_NAME); //change to your own index name
+
+    //embed the PDF documents
+    await PineconeStore.fromDocuments(docs, embeddings, {
+      pineconeIndex: index,
+      namespace: PINECONE_NAME_SPACE,
+      textKey: 'text',
+    });
+  } catch (error) {
     console.log('error', error);
-    throw new Error('Failed to ingest documents');
+    throw new Error('Failed to ingest your data');
   }
 };
 
 (async () => {
   await run();
-  console.log("done");
+  console.log('ingestion complete');
 })();
